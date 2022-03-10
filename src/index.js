b487451ef9b99355c260f80829729f6ae91ca991
Todd Heckeler <todd@consumedesign.com>
2022-03-10 09:57:34 -0500

    index

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import Head from "./templates/Head";
-ReactDOM.render(<Head />, document.getElementById("head"));
 
 import Header from "./templates/Header";
 import Footer from "./templates/Footer";
+
+ReactDOM.render(<Head />, document.getElementById("head"));
 ReactDOM.render(<Header />, document.getElementById("header"));
 ReactDOM.render(<Footer />, document.getElementById("footer"));
